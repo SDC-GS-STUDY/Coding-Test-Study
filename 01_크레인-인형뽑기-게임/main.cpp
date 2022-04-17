d10dbd7f221e92b4ca1306f302d3558a78129ae8
Marey <kamin_s2@g.hongik.ac.kr>
2022-04-17 19:53:02 +0900

    Update main.cpp

--- 01_크레인-인형뽑기-게임/main.cpp
+++ 01_크레인-인형뽑기-게임/main.cpp
@@ -5,56 +5,86 @@
 
 using namespace std;
 
-int picked(int index, vector<vector<int>>& board)
-{    
-    for (int i = 0; i < board.size(); i++)
+class User
+{
+private:
+   vector<int> inputBuff; //moves
+   int pckItem;//뽑은 인형
+public:
+    User(vector<int> _inputBuff)
+        :inputBuff(_inputBuff),pckItem(0)
     {
-        if (board[i][index - 1] != 0)//행[열index] 로 비어있지 않으면
-        {
-            int ret; //return 값 임시 저장
-            ret = board[i][index - 1];
-            board[i][index - 1] = 0;//꺼내졌으므로 해당자리는 0
-            return ret;//리턴!
-        }
+
     }
 
-    return 0;
-}
-
-int solution(vector<vector<int>> board, vector<int> moves) {
-    int answer = 0;
-    //vector<int> basket;
-    stack<int> basket;
-
-    //바구니에 담기
-    for (auto index : moves)
+    bool pick(vector<vector<int>>& _board) //인형뽑기에 성공하면 true, 실패하면 false
     {
-        int doll = picked(index, board);
+        int index = inputBuff[0];
+        inputBuff.erase(inputBuff.begin());
 
-        if (doll == 0) continue;
-        
-        if(basket.empty()) basket.push(doll);
-        else
+        for (int i = 0; i < _board.size(); i++)
         {
-            if (basket.top() == doll)
+            if (_board[i][index - 1] != 0)//행[열index] 로 비어있지 않으면
+            {
+                int ret; //return 값 임시 저장
+                pckItem = _board[i][index - 1];
+                _board[i][index - 1] = 0;//꺼내졌으므로 해당자리는 0
+
+                return true;//리턴!
+            }
+        }
+        return false;
+    }
+
+    const int getpckItem() { return pckItem; }
+
+    const bool isEnd()
+    {
+        if (inputBuff.empty())
+            return false;
+        return true;
+    }
+};
+
+class Machine
+{
+private:
+    vector<vector<int>> board;
+    stack<int> basket;
+    
+    User* user;
+
+    int answer;
+public:
+    Machine(User* _user, vector<vector<int>> board):user(_user),answer(0)
+    {
+
+    }
+
+    void update()
+    {
+        while (user->isEnd())
+        {
+            if (user->pick(board)) continue;
+
+            if (!basket.empty() && basket.top() == user->getpckItem())
             {
                 basket.pop();
                 answer += 2;
             }
             else
-                basket.push(doll);
+                basket.push(user->getpckItem());
         }
-   
+
+        cout << answer << endl;
     }
+};
 
 
-    
-    return answer;
-}//[[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
-//0 비어있음
-//1 네오 : 2 무지 : 3 튜브 : 4 어피치 : 5 프로도
-//4311324
 int main()
 {
-    cout<<solution({ {0, 0, 0, 0, 0},{0, 0, 1, 0, 3},{0, 2, 5, 0, 1},{4, 2, 4, 4, 2},{3, 5, 1, 3, 1} }, { 1, 5, 3, 5, 1, 2, 1, 4 })<<endl;
+    User* user = new User({ 1, 5, 3, 5, 1, 2, 1, 4 });
+    Machine* game = new Machine(user, { {0, 0, 0, 0, 0},{0, 0, 1, 0, 3},{0, 2, 5, 0, 1},{4, 2, 4, 4, 2},{3, 5, 1, 3, 1} });
+
+    game->update();
 }
