db2733f63332eb1fd76b6f44239ec17db1410400
Marey <kamin_s2@g.hongik.ac.kr>
2022-05-02 17:59:40 +0900

    Revert "Revert "Merge pull request #11 from Yeram522/main""
    This reverts commit 19f92f996a2c5ef803276d9265e7f968eebf849e.

--- 01_크레인-인형뽑기-게임/main.cpp
+++ 01_크레인-인형뽑기-게임/main.cpp
@@ -12,15 +12,14 @@ private:
    int pckItem;//뽑은 인형
 public:
     User(vector<int> _inputBuff)
-        :inputBuff(_inputBuff),pckItem(0)
-    {
+        :inputBuff(_inputBuff),pckItem(0) {}
 
-    }
+    ~User(){}
 
-    bool pick(vector<vector<int>>& _board) //인형뽑기에 성공하면 true, 실패하면 false
+    const bool pick(vector<vector<int>>& _board) //인형뽑기에 성공하면 true, 실패하면 false
     {
-        int index = inputBuff[0];
-        inputBuff.erase(inputBuff.begin());
+        int index = inputBuff[0]; //input값 맨 앞의 위치를 지역변수에 담는다.
+        inputBuff.erase(inputBuff.begin());// 담았으니까 버퍼에서 삭제해준다.
 
         for (int i = 0; i < _board.size(); i++)
         {
@@ -40,7 +39,7 @@ public:
 
     const bool isEnd()
     {
-        if (inputBuff.empty())
+        if (inputBuff.empty()) //user의 인풋값 버퍼가 비면 false 리턴
             return false;
         return true;
     }
@@ -51,32 +50,39 @@ class Machine
 private:
     vector<vector<int>> board;
     stack<int> basket;
-    
+
     User* user;
 
     int answer;
 public:
-    Machine(User* _user, vector<vector<int>> _board):user(_user),answer(0)
+    Machine(User* _user, vector<vector<int>> _board) 
+        :user(_user), answer(0), board(_board){}
+
+    ~Machine()
     {
-        board = _board;
+        delete user;
+        user = 0;
     }
 
-    void update()
+    void play()
     {
-        while (user->isEnd())
+        while (user->isEnd()) //user의 input stack이 empty가 될 떄 까지 반복
         {
-            if (!user->pick(board)) continue;
-
+            if (!user->pick(board)) continue; //input에 따라 user가 인형을 뽑는 데, 위치에 인형이 있으면 true, 없으면 false 반환됨.
+            //안뽑혔으면 아래 명령을 수행할 필요가 없어서 continue
+            //뽑혔으면, basket 확인해서 콤보인지 확인한다.
+            //바구니가 비지 않은 상태에서 스택 상단과 비교.
             if (!basket.empty() && basket.top() == user->getpckItem())
             {
                 basket.pop();
                 answer += 2;
             }
             else
-                basket.push(user->getpckItem());
+                basket.push(user->getpckItem()); //바구니가 비어있거나, 콤보가 아니면 그냥 바구니에 인형을 담는다.
             
         }
 
+        //while문이 종료 되면 최종 값 print
         cout << answer << endl;
     }
 };
@@ -87,5 +93,7 @@ int main()
     User* user = new User({ 1, 5, 3, 5, 1, 2, 1, 4 });
     Machine* game = new Machine(user, { {0, 0, 0, 0, 0},{0, 0, 1, 0, 3},{0, 2, 5, 0, 1},{4, 2, 4, 4, 2},{3, 5, 1, 3, 1} });
 
-    game->update();
+    game->play();
+
+    return 0;
 }
---
+++ 03_오픈채팅방/03_오픈채팅방/main.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+using namespace std;
+
+void DebugLog(vector<pair<int, pair<string, string>>> log_table)
+{
+	cout << "===========Update===========" << endl;
+	for (auto log : log_table)
+	{
+		std::cout << "State:" << log.first << "    ID:" << log.second.first << "    Name:" << log.second.second << endl;
+	}
+
+	return;
+}
+
+vector<string> solution(vector<string> record) {
+	vector<string> answer;
+
+	vector<pair<int, pair<string, string>>> log_table;
+	int table_idx = 0;
+
+	int idx = 0;
+	int bnk_cnt = 0;
+
+	vector<pair<string, string>> m_joinLog; //채팅방에 참여한 이력 또는 현재 있는 member
+	//Data pharsing and Update
+	for (auto rec : record)  //record -> sentence
+	{
+		pair<string, string> user_info;
+		int state = 0; // Enter = 0; Leave = 1, Chage = 2;
+		for (int i = 0; i < rec.size(); i++)// sentence-> char
+		{
+			if (rec[i] != ' ') continue;
+			switch (bnk_cnt)
+			{
+			case 0: //Enter,Exit
+				if (rec.substr(idx, i) == "Enter") state = 0;
+				else if (rec.substr(idx, i) == "Leave")
+				{
+					state = 1;
+					user_info.first = rec.substr(idx+i+1, rec.size() - idx+1);
+					user_info.second = "none";
+				}
+				else if (rec.substr(idx, i) == "Change") state = 2;
+				idx = i + 1;//공백 다음 index
+				break;
+			case 1:
+				user_info.first = rec.substr(idx, i - idx);
+				user_info.second = "none";
+				
+				idx = i + 1;//공백 다음 index
+				user_info.second = rec.substr(idx, rec.size()- idx);
+				break;
+			}
+			bnk_cnt++;
+		}
+		log_table.push_back({ state, {user_info} });
+
+		//debug.Data Log
+		std::cout << "State:" << state << "    ID:" << user_info.first << "    Name:" << user_info.second << endl;
+
+		//Upate Data : 이름이 바뀌면 이전 기록 이름도 바뀐다. 1. 나가서 이름 바꿔올 경우 2. 내부에서 이름을 바꿀 경우
+     	//1. Check new member ID is it prev member. 
+	    // 1-1. Check is name changed
+		if (state == 0)
+		{
+			//이전 멤버인지 아닌지 검사
+			bool isOrigin = false;		
+			for (auto member : m_joinLog)
+			{
+				if (member.first == user_info.first)
+				{
+					isOrigin = true;
+					break;
+				}
+			}
+
+			//채팅방에 새로 들어온 사람이 이전 멤버일 경우
+			if (isOrigin)
+			{
+				for (auto& info : log_table)
+				{
+					if (info.second.first == user_info.first && info.second.second != user_info.second)
+					{
+						info.second.second = user_info.second; //change to new name
+						DebugLog(log_table);
+					}
+				}
+			}
+			else
+			{
+				m_joinLog.push_back(user_info);
+			}
+
+		}
+		else if (state == 2)  //2. Change the name in chat room.  -> just change all name
+		{
+			for (auto& info : log_table)
+			{
+				if (info.second.first == user_info.first)
+				{
+					info.second.second = user_info.second; //change to new name
+					DebugLog(log_table);
+				}
+			}
+		}
+
+
+
+		idx = 0;
+		bnk_cnt = 0;
+		state = 0;
+
+		table_idx++;
+	}
+
+	cout << "RESULT LOG" << endl;
+	//Printing Result
+	for (auto log : log_table)
+	{
+		int state = log.first;
+		string s_log;
+		s_log += log.second.second; // 000
+		s_log += "님이 ";
+
+		if (state == 0)
+		{
+			s_log += "들어왔습니다.";
+		}
+		else if (state == 1)
+		{
+			s_log += "나갔습니다.";
+		}
+		else
+			continue;
+
+		answer.push_back(s_log);
+
+		cout << s_log << endl;
+	}
+
+
+	return answer;
+}
+
+int main()
+{
+	solution({ "Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan" });
+}
