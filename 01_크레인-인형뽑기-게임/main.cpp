19f92f996a2c5ef803276d9265e7f968eebf849e
Marey <kamin_s2@g.hongik.ac.kr>
2022-05-02 17:55:53 +0900

    Revert "Merge pull request #11 from Yeram522/main"
    This reverts commit b21b2ad8c47f992dfa0b89234a25f16ca3f2c34e, reversing
    changes made to 2b19ecbf3f4666714240a0f3a5f028a8f3e0108c.

--- 01_크레인-인형뽑기-게임/main.cpp
+++ 01_크레인-인형뽑기-게임/main.cpp
@@ -12,14 +12,15 @@ private:
    int pckItem;//뽑은 인형
 public:
     User(vector<int> _inputBuff)
-        :inputBuff(_inputBuff),pckItem(0) {}
-
-    ~User(){}
-
-    const bool pick(vector<vector<int>>& _board) //인형뽑기에 성공하면 true, 실패하면 false
+        :inputBuff(_inputBuff),pckItem(0)
     {
-        int index = inputBuff[0]; //input값 맨 앞의 위치를 지역변수에 담는다.
-        inputBuff.erase(inputBuff.begin());// 담았으니까 버퍼에서 삭제해준다.
+
+    }
+
+    bool pick(vector<vector<int>>& _board) //인형뽑기에 성공하면 true, 실패하면 false
+    {
+        int index = inputBuff[0];
+        inputBuff.erase(inputBuff.begin());
 
         for (int i = 0; i < _board.size(); i++)
         {
@@ -39,7 +40,7 @@ public:
 
     const bool isEnd()
     {
-        if (inputBuff.empty()) //user의 인풋값 버퍼가 비면 false 리턴
+        if (inputBuff.empty())
             return false;
         return true;
     }
@@ -50,39 +51,32 @@ class Machine
 private:
     vector<vector<int>> board;
     stack<int> basket;
-
+    
     User* user;
 
     int answer;
 public:
-    Machine(User* _user, vector<vector<int>> _board) 
-        :user(_user), answer(0), board(_board){}
-
-    ~Machine()
+    Machine(User* _user, vector<vector<int>> _board):user(_user),answer(0)
     {
-        delete user;
-        user = 0;
+        board = _board;
     }
 
-    void play()
+    void update()
     {
-        while (user->isEnd()) //user의 input stack이 empty가 될 떄 까지 반복
+        while (user->isEnd())
         {
-            if (!user->pick(board)) continue; //input에 따라 user가 인형을 뽑는 데, 위치에 인형이 있으면 true, 없으면 false 반환됨.
-            //안뽑혔으면 아래 명령을 수행할 필요가 없어서 continue
-            //뽑혔으면, basket 확인해서 콤보인지 확인한다.
-            //바구니가 비지 않은 상태에서 스택 상단과 비교.
+            if (!user->pick(board)) continue;
+
             if (!basket.empty() && basket.top() == user->getpckItem())
             {
                 basket.pop();
                 answer += 2;
             }
             else
-                basket.push(user->getpckItem()); //바구니가 비어있거나, 콤보가 아니면 그냥 바구니에 인형을 담는다.
+                basket.push(user->getpckItem());
             
         }
 
-        //while문이 종료 되면 최종 값 print
         cout << answer << endl;
     }
 };
@@ -93,7 +87,5 @@ int main()
     User* user = new User({ 1, 5, 3, 5, 1, 2, 1, 4 });
     Machine* game = new Machine(user, { {0, 0, 0, 0, 0},{0, 0, 1, 0, 3},{0, 2, 5, 0, 1},{4, 2, 4, 4, 2},{3, 5, 1, 3, 1} });
 
-    game->play();
-
-    return 0;
+    game->update();
 }
